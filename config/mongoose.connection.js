bb30c6336e8033f5d07ecd9727d7f6dc1df5adab
vaibhav singh <vaibhav.anmol.chandel@gmail.com>
2024-08-08 14:36:48 +0530

    final-touch

--- config/mongoose.connection.js
+++ config/mongoose.connection.js
@@ -3,6 +3,7 @@ const config = require('config');
 // const debug = require("debug")("development:mongoose");
 
 mongoose.connect(`${config.get("MONGODB_URI")}/ecommerce`)
+    // mongoose.connect(`mongodb://127.0.0.1:27017/ecommerce`)
     .then(function () {
         console.log("connect");
     })
